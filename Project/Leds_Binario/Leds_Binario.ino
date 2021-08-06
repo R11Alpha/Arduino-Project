a05868596896d4c0e3d4367a874846e0bd48cc56
R11Alpha <canchemario11@gmail.com>
2021-08-06 09:36:56 -0500

    Solve problems with Arduino

--- Project/Leds_Binario/Leds_Binario.ino
+++ Project/Leds_Binario/Leds_Binario.ino
@@ -1,17 +1,17 @@
-#include <math.h> //biblioteca que incluye funciones matemáticas
+#include <math.h> //Biblioteca que se necesita para hacer funciones matematicas
 int numero,  digito, cont;
-double binario, ex, base;//variables para hacer funciones de numeros decimales
-int leds[4];//matriz de 4 led
+double binario, ex, base;//Variables para poder hacer funciones con decimales
+int leds[4];//Matriz de leds
 
-int led1 = 3, led2 = 4, led3 = 5, led4 = 6; //variables con su numero de led
+int led1 = 3, led2 = 4, led3 = 5, led4 = 6; //Asignación de cada led a su respectiva salida
 
 void setup() {
   Serial.begin(9600);
-  pinMode(led1, OUTPUT);
+  pinMode(led1, OUTPUT); //Introducimos el tipo de cada led en este caso como salida
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
   pinMode(led4, OUTPUT);
-  Serial.println("introduce un numero");
+  Serial.println("Introduce un numero"); //Impresión en el monitor para introducir el número
 
 }
 
@@ -20,8 +20,8 @@ void loop() {
 
     do {
 
-      numero = Serial.parseInt();//convertimos el numero dato ingresado a numero entero
-    } while (numero < 0); //condicion while que
+      numero = Serial.parseInt();//Convierte el número decimal a número entero
+    } while (numero < 0); //Condición While
     base = 10;
     ex = 0;
     cont = 0;
@@ -46,7 +46,7 @@ void loop() {
     digitalWrite(led1, leds[0]);
 
     delay(1000);
-    Serial.println("introduce un numero");
+    Serial.println("Introduce un numero");
   }
 }
 
